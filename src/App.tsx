0f5ab031435f6dca808e518892f6391ffb68c2b7
Lucas Santos Silva <lucassantos.santos90@hotmail.com>
2025-10-22 17:37:29 -0300

    Adding ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
 import { useState } from "react";
 import "./App.css";
 
+interface TaskProps {
+  title: string;
+}
+
 function App() {
-  const [task, setTask] = useState([]);
+  const [tasks, setTask] = useState<TaskProps[]>([]);
   const [inputValue, setInputValue] = useState("");
 
-  function handleChange(e) {
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setInputValue(e.target.value);
   }
 
-  function handleAddTask(e) {
+  function handleAddTask(e: React.SyntheticEvent<HTMLFormElement>) {
     e.preventDefault();
     if (inputValue.trim() === "") return;
-    setTask(inputValue);
+    setTask([...tasks, { title: inputValue }]);
     setInputValue("");
   }
   return (
     <div>
       <h1>To do list</h1>0 task added
       <div>
-        <input type="checkbox" />
-        <input type="text" onChange={handleChange} />{" "}
-        <button type="submit" onClick={handleAddTask}>
-          +
-        </button>
+        <form onSubmit={handleAddTask}>
+          <input type="checkbox" />
+          <input type="text" onChange={handleChange} />{" "}
+          <button type="submit">+</button>
+        </form>
       </div>
       <div>
         <button>All</button>
         <button>Active</button>
         <button>Done</button>
-        {task ? <p>{task}</p> : <p>There's no task added</p>}
+        <div>
+          {tasks ? (
+            <ul>
+              <p>
+                {tasks.map((task) => (
+                  <li>{task.title}</li>
+                ))}
+              </p>
+            </ul>
+          ) : (
+            <p>There's no task added</p>
+          )}
+        </div>
       </div>
     </div>
   );
